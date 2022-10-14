c688fe7bdd3bf5ad19f95c28e84498a3773b8c9d
dalenti <114300375+dalenti@users.noreply.github.com>
2022-10-14 23:05:21 +0700

    Add files via upload

---
+++ HandPhoneNo4.java
@@ -0,0 +1,25 @@
+
+public class HandPhoneNo4 {
+    String jenis_hp;
+    int tahun_pembuatan;
+
+    public void setDataHP(String jenis_hp, int tahun_pembuatan){
+        this.jenis_hp = jenis_hp;
+        this.tahun_pembuatan = tahun_pembuatan;
+    }
+
+    public String getJenisHP(){
+        return jenis_hp;
+    }
+
+    public int getTahunPembuatan(){
+        return tahun_pembuatan;
+    }
+
+    public static void main(String[] args) {
+        HandPhone hp = new HandPhone();
+        hp.setDataHP(jenis_hp, tahun_pembuatan);
+        System.out.println("Jenis hp: " + hp.jenis_hp);
+        System.out.println("tahun pembuatan: " + hp.getTahunPembuatan());
+    }
+}
